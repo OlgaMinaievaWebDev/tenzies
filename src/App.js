a7ac392d4fbe791e3bd1792584ba1390ac5e4a1e
Olga <minaeva9@gmail.com>
2024-04-19 14:15:34 -0400

    hard coded random numbers

--- src/App.js
+++ src/App.js
@@ -1,3 +1,32 @@
+import Die from "./Die";
 export default function App() {
-  return <main></main>;
+  function allNewDice() {
+    const newDice = [];
+
+    for (let i = 0; i < 10; i++) {
+      const randomDie = Math.ceil(Math.random() * 6);
+      newDice.push(randomDie);
+    }
+    return newDice;
+  }
+
+  console.log(allNewDice());
+  return (
+    <>
+      <main>
+        <div className="dice-container">
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+        </div>
+      </main>
+    </>
+  );
 }
